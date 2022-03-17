445af7b5f0ab556ba82cbe40955fd2e5063acd1d
Ikhsan Nurdiansyah <ikhsannurdiansyah78@gmail.com>
2022-03-17 18:14:22 +0700

    while

---
+++ while.py
@@ -0,0 +1,14 @@
+
+angka = 1
+tambah = 0
+
+print("Masukkan angka untuk ditambahkan ")
+print("Masukkan angka 0 untuk keluar")
+
+while angka != 0:
+    print("Jumlah: ", tambah)
+    angka = int(input("Masukkan angka :"))
+    tambah = tambah + angka
+
+    print("Jumlah total = ", tambah)
+print("GOODBYEEEE!!!!!")
